9487c7d94ac7e2588fb1155b5732d153d0fca44b
Ryo Akagi <ryoakg@gmail.com>
2016-01-28 13:43:57 +0900

    Delete dependencies for new Clojure versions.

--- project.clj
+++ project.clj
@@ -1,5 +1,2 @@
 (defproject clj-growl "0.2.1"
-  :description "Send Growl notifications from Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
-  :dev-dependencies [[jline "0.9.94"]])
+  :description "Send Growl notifications from Clojure.")
